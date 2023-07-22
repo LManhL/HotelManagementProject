f0c9ecf06b882ced22a1792ddf8cce9bc8a7219d
LManhL <manhmap18112002@gmail.com>
2023-07-22 14:49:54 +0700

    new update 22/7

--- app/src/main/java/com/example/hotelmangementproject/ui/account/AccountFragment.java
+++ app/src/main/java/com/example/hotelmangementproject/ui/account/AccountFragment.java
@@ -7,12 +7,18 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.hotelmangementproject.databinding.FragAcMainBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class AccountFragment extends Fragment {
@@ -26,12 +32,45 @@ public class AccountFragment extends Fragment {
 
         binding = FragAcMainBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-        final TextView textView = binding.textAccount;
-        accountViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        binding.btnChangePasswordAcMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(checkValidate()){
+                    changePassword();
+                }
+            }
+        });
         return root;
     }
 
+    private boolean checkValidate(){
+        if(binding.edtNewpasswordAcMain.getText().toString().isEmpty()){
+            binding.edtNewpasswordAcMain.setError("Required");
+            return false;
+        }
+        if(binding.edtConfirmNewpasswordAcMain.getText().toString().isEmpty()){
+            binding.edtConfirmNewpasswordAcMain.setError("Required");
+            return false;
+        }
+        if(!binding.edtConfirmNewpasswordAcMain.getText().toString().equals(binding.edtNewpasswordAcMain.getText().toString())){
+            Toast.makeText(getActivity(), "Two passwords are not the same", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+    private void changePassword(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        user.updatePassword(binding.edtNewpasswordAcMain.getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Log.d("Change password","Success");
+                }
+                binding.edtConfirmNewpasswordAcMain.setText("");
+                binding.edtNewpasswordAcMain.setText("");
+            }
+        });
+    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
--- app/src/main/java/com/example/hotelmangementproject/ui/account/AccountViewModel.java
+++ app/src/main/java/com/example/hotelmangementproject/ui/account/AccountViewModel.java
@@ -5,14 +5,5 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class AccountViewModel extends ViewModel {
-    private final MutableLiveData<String> mText;
 
-    public AccountViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is account fragment");
-    }
-
-    public LiveData<String> getText() {
-        return mText;
-    }
 }
